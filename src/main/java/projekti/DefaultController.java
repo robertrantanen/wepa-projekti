ea5b2d69aea580ffd19fcc35b913a076c6706b15
Robert <robert.j.rantanen@helsinki.fi>
2020-04-12 15:21:47 +0300

    kirjautuminen

--- src/main/java/projekti/DefaultController.java
+++ src/main/java/projekti/DefaultController.java
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DefaultController {
 
-    @GetMapping("*")
+    @GetMapping("/")
     public String helloWorld(Model model) {
-        model.addAttribute("message", "World!");
         return "index";
     }
 }
