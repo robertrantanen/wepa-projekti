b04b08eb8fd514305a27bd5e0bfc52cac1785e1f
Robert <robert.j.rantanen@helsinki.fi>
2020-05-03 14:57:13 +0300

    yhteyksien poistaminen

--- src/main/java/projekti/ConnectionController.java
+++ src/main/java/projekti/ConnectionController.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,6 +56,12 @@ public class ConnectionController {
         Connection connection = connectionRepository.getOne(id);
         connection.setAccepted(true);
         connectionRepository.save(connection);
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/connections/{id}")
+    public String remove(@PathVariable Long id) {
+        connectionRepository.deleteById(id);
         return "redirect:/";
     }
 
